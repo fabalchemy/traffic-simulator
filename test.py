5d0d211f7b50d030c18dcbcda0c26ee7dd655eda
ktroles <46374729+ktroles@users.noreply.github.com>
2019-01-04 14:56:05 +0100

    C'est quoi ça ?
    Co-Authored-By: null <FabAlchemy@users.noreply.github.com>

---
+++ test.py
@@ -0,0 +1,11 @@
+"""Ceci est un test de synchronisation"""
+"""Ce fichier regroupe les classes nécessaires pour notre graphe"""
+
+class Route:
+    """Classe créant une route entre deux sommets"""
+
+    def __init__(self,nom,s1,s2):
+        """Route reliant les sommets s1 et s2"""
+
+        self.nom = nom
+        
