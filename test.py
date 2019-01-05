c460b5f49fab6cef47e35af1a40fcb4dad7c4fc0
Fabien SAVY <fab.alchemy@gmail.com>
2019-01-05 18:49:24 +0100

    Modif by @ktroles
    Co-Authored-By: null <ktroles@users.noreply.github.com>

--- test.py
+++ test.py
@@ -14,8 +14,23 @@ class Road:
 class Cross:
     """Class creating a cross of different roads
 
-    Objetc name should be : bor006 or tal152"""
+    Objet name should be : bor006 or tal152"""
 
-    def __init__(self):
-        """Cross of different roads
-        """
+    def __init__(self, roads_list, flux_mat, duration_light_mat):
+        """Cross of different roads defined by:
+        - List of roads in/outcoming
+        - Matrix of flux (number /time unit , origin/destination)(where and how many cars from road 1 are going to road 2,3 ...)
+        - Matrix of duration of each traffic light"""
+
+        # We verify the cross name is as wanted 'abc123'
+        if len(self.name) != 6:
+            raise NameError("Object name must have 'abc123' form")
+        for i in range(6):
+            if i // 3 == 0:
+                if self.name[i] not in alphabet:
+                    raise NameError("Object name must have 'abc123' form")
+            else:
+                try:
+                    val = int(self.name[i])
+                except ValueError:
+                    raise NameError("Object name must have 'abc123' form")
