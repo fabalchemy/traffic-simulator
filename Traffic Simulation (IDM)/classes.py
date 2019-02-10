a7935c3d43d3d1ad099d1143796caba6598b030b
ktroles <46374729+ktroles@users.noreply.github.com>
2019-02-10 17:00:42 +0100

    Working on classes.py to have a map simulation

--- Traffic Simulation (IDM)/classes.py
+++ Traffic Simulation (IDM)/classes.py
@@ -7,7 +7,8 @@ class Road:
     """Class modelizing a road between two crosses"""
 
     notVehicleError = TypeError("The object added to the road must be a Vehicle")
-    notCrossError = TypeError("Input origin_cross is not type Cross")
+    notCrossError = TypeError("Input cross is not type Cross")
+    crossNotOnRoad = ValueError("This road is not linked to the input cross")
 
     def __init__(self, name, cross1, cross2, speed_limit):
         self.name = name
@@ -35,7 +36,7 @@ class Road:
         if type(origin_cross) is not Cross: #Does it work for classes heriting from Cross ?
             raise notCrossError
         if origin_cross not in [self.cross1,self.cross2]:
-            return ValueError("This road is not linked to the input cross")
+            return crossNotOnRoad
 
         #Then we add the vehicle at the beginning of the road, in the corresponding direction
         if origin_cross == self.cross1:
@@ -52,40 +53,69 @@ class Road:
 
         if vehicle in self.vehic
 
-    def __add__(self,vehicle):
-        """To add a vehicle on the road"""
-        if type(vehicle) is not Vehicle:
-            raise notVehicleError
 
-        vehicles_list = vehicle + vehicles_list
 
-    def __radd__(self,vehicle):
-        """Same action in the two ways"""
-        self + vehicle
+    def leader(self,destination_cross):
+        """Return the first vehicle to arrive on the destination cross by this road"""
 
-    def __del__(self):
-        """Delete and return the last vehicle on the road"""
-        if type(vehicle) is not Vehicle:
-            raise notVehicleError
+        #input parameters check
+        if type(destination_cross) is not Cross:
+            raise notCrossError
+        if destination_cross not in [self.cross1,self.cross2]:
+            raise crossNotOnRoad
 
-        return vehicles_list.pop()
-
-    def leader(self):
-        """Return the leader"""
+        if destination_cross is self.cross1: # return the first vehicle arriving to the destination cross from this road
+            return self.vehicles_list_21[-1]
+        else:
+            return self.vehicles_list_12[-1]
 
 class Cross:
     """Class modelizing a cross"""
-    def __init__(self, coords, cross_type):
+    def __init__(self, coords, cross_type, cross_dispatch_matrix = list()):
         """(cartesian) coords : (x,y)
-        cross_type : 'trafficLight', 'generator', 'simpleCross'"""
+        cross_type : ['trafficLight', 'generator', 'simpleCross']
+        cross_dispatch_matrix : list of lists of vehicles dispatch on other roads
+            (L[x] = entry, L[x][y] exit)"""
+
+        # Check valid coords
         if not(type(coords) is tuple and len(coords) == 2 and type(coords[0]) in (int,float) and
         type(coords[1]) in (int,float)):
             raise TypeError("Types of entered parameters are incorrect")
         if cross_type not in ["trafficLight", "generator", "simpleCross"]:
             raise ValueError("Incorrect cross type")
-
         self.coords = coords
+
+        # Check cross_type
+        if type(cross_type) is not str:
+            raise TypeError("String type expected for cross_type")
+        if cross_type not in ['trafficLight', 'generator', 'simpleCross']:
+            raise ValueError("Input cross_type is invalid")
         self.cross_type = cross_type
+
+        # Check cross_dispatch_matrix
+        if type(cross_dispatch_matrix) is not list:
+            raise TypeError("cross_dispatch_matrix must be list type")
+
+        # Check cross_dispatch_matrix is list of lists of the same lenght (same number of incoming and outgoing roads)
+        roads_nb = len(cross_dispatch_matrix)
+        for road in range(roads_nb):
+            if type(cross_dispatch_matrix[road]) is not list:
+                raise TypeError("cross_dispatch_matrix must be a list of lists")
+            if len(cross_dispatch_matrix[road]) != roads_nb:
+                raise ValueError("cross_dispatch_matrix must have the same number of incoming and outgoing roads")
+
+            # By our own choice, cars cannot turn back when arriving to a cross
+            # This involves for all incoming road, cross_dispatch_matrix[i][i] must equal 0
+            if cross_dispatch_matrix[road][road] != 0:
+                raise ValueError("Vehicles cannot turn back at a cross. This involves cross_dispatch_matrix[road i][road i] must be 0 for every road.")
+
+
+
+
+
+        self.cross_dispatch_matrix = cross_dispatch_matrix
+
+
 
 class TrafficLight(Cross):
     """Traffic light, a type of cross"""
@@ -110,10 +140,10 @@ class TrafficLight(Cross):
 class Vehicle:
     """Vehicle"""
 
-    nb_vehicle = 0
-    def __init__(self,road,T, leader, s0, a = 1, vehicle_type = 0, b = 1.5):
-        """Class modelizing a car
+    def __init__(self,road,origin_cross,T, leader, s0, a = 1, vehicle_type = 0, b = 1.5):
+        """Class modelizing a car:
         road
+        origin_cross : Cross by where the car enter on the road
         T : desired time headway (security time) [s]
         leader : vehicle ahead
         s0 = minimal distance (bumper-to-bumper) to the leader [m]
@@ -128,7 +158,6 @@ class Vehicle:
 
         # TODO: Be more specific about that problem
 
-        Vehicle.nb_vehicle += 1
         self.road = road
         self.T = T
         self.leader = leader
@@ -140,7 +169,6 @@ class Vehicle:
         elif vehicle_type == 1 : # It's a truck
             self.b_max = 4
         else:
-            Vehicle.nb_vehicle -= 1
             raise TypeError("Non existing vehicle. 0 = car, 1 = truck")
 
         self.b = b
